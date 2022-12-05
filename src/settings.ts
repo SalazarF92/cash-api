5edf82845a07a95b858296c7b32e82648ebcd148
Flavio Salazar <flavio.campinas92@gmail.com>
2022-12-05 16:05:32 -0300

    fix(postgres): envs

--- src/settings.ts
+++ src/settings.ts
@@ -1,7 +1,7 @@
 export const JWT_SECRET = process.env.JWT_SECRET || "undefined";
 export const PORT = process.env.PORT || 3000;
-export const DB_USER = process.env.DB_USER;
-export const DB_PASSWORD = process.env.DB_PASSWORD;
-export const DB_NAME = process.env.DB_NAME;
-export const DB_PORT = Number(process.env.DB_PORT);
-export const DB_HOST =  process.env.DB_HOST || "localhost";
+export const PGUSER = process.env.PGUSER;
+export const PGPASSWORD = process.env.PGPASSWORD;
+export const PGDATABASE = process.env.PGDATABASE;
+export const PGPORT = Number(process.env.PGPORT);
+export const PGHOST =  process.env.PGHOST || "localhost";
