02f016967222f98dffa76e9933cef160d4b87af2
Flavio Salazar <flavio.campinas92@gmail.com>
2022-12-16 08:59:32 -0300

    test: queue

---
+++ src/tests/queue.test.ts
@@ -0,0 +1,37 @@
+import { InMemoryQueueService } from "./services/in-memory-queue-service";
+
+const props = {
+  id: "id",
+  queue: "queue",
+  topic: "topic",
+  exchange: "exchange",
+  message: {
+    id: "id",
+    body: "message",
+  },
+};
+const queueService = new InMemoryQueueService();
+
+describe("QueueService", () => {
+  it("should publish to queue", async () => {
+    const queue = await queueService.publishInQueue(props);
+    expect(queue).toBeTruthy();
+    expect(queue.message.body).toContain("message");
+    expect(queue.queue).toContain("queue");
+    expect(typeof queue.queue).toBe(typeof "string");
+    expect(queue.topic).toContain("topic");
+    expect(typeof queue.topic).toBe(typeof "string");
+  });
+
+  it("should publish to exchange", async () => {
+    const queue = await queueService.publishInExchange(props);
+    expect(queue).toBeTruthy();
+    expect(queue.exchange).toBe("exchange");
+    expect(typeof queue.exchange).toBe(typeof "string");
+    expect(queue.message.body).toContain("message");
+    expect(queue.queue).toContain("queue");
+    expect(typeof queue.queue).toBe(typeof "string");
+    expect(queue.topic).toContain("topic");
+    expect(typeof queue.topic).toBe(typeof "string");
+  });
+});
